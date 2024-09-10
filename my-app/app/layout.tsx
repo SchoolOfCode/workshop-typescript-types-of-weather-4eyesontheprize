7e4d13f46bd1f7b996aceef4427ac1724916317e
Lana Maugé <lanamauge@gmail.com>
2024-09-10 15:10:13 +0100

    attempted to import page function to layout.tsx

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
+import Page from "./leaderboard/page.tsx"
 import Link from "next/link";
 import "./globals.css";
 
